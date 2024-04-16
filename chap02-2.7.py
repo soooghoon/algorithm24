8e127a7fcfc73e66b226fae26a5b425703c93a81
soooghoon <162541427+soooghoon@users.noreply.github.com>
2024-04-16 10:44:43 +0900

    Delete chap02-2.7.py

--- chap02-2.7.py
+++
@@ -1,9 +0,0 @@
-def hanoi_tower(n, fr, tmp, to):
-    if n == 1:
-        print("원판 1: %s --> %s" % (fr, to))
-    else:
-        hanoi_tower(n-1, fr, to, tmp)
-        print("원판 %d: %s --> %s" % (n, fr, to))
-        hanoi_tower(n-1, tmp, fr, to)
-
-hanoi_tower(4, 'a', 'b', 'c')
