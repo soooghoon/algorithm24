af3e357fc0fe8119e425789720336fec01e61279
soooghoon <162541427+soooghoon@users.noreply.github.com>
2024-04-16 10:43:26 +0900

    Add files via upload

---
+++ AL/chap03-01.py
@@ -0,0 +1,18 @@
+def printStep(arr, val):
+    print(" Step %2d = " % val, end='')
+    print(arr)
+
+def selection_sort(A) :
+    n = len(A)
+    for i in range(n-1):
+        least = i
+        for j in range(i+1, n):
+            if (A[j] < A[least]):
+                least = j
+        A[i], A[least] = A[least], A[i]
+        printStep(A, i+1)
+
+data = [5, 3, 8, 4, 9, 1, 6, 2, 7]
+print("Original : ", data)
+selection_sort(data)
+print("Selection : ", data)
